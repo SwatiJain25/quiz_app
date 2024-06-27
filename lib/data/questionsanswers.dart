b69646c9639857db7bdcb2428dac36a314bdad22
SwatiJain25 <swatijain2021s@gmail.com>
2024-06-27 16:28:16 +0530

    newques

--- lib/data/questionsanswers.dart
+++ lib/data/questionsanswers.dart
@@ -21,4 +21,34 @@ QuizQuestion('Window is to pane as book is to',[
   'ounce','gallon','milk','liquid'
     ]
   ),
+   QuizQuestion('Pen is to writer as brush is to', [
+    'painter', 'artist', 'canvas', 'ink'
+  ]),
+  QuizQuestion('Sun is to day as moon is to', [
+    'night', 'star', 'planet', 'space'
+  ]),
+  QuizQuestion('Artist is to painting as chef is to', [
+    'cooking', 'restaurant', 'food', 'recipe'
+  ]),
+  QuizQuestion('Keyboard is to typing as calculator is to', [
+    'math', 'numbers', 'accounting', 'keys'
+  ]),
+  QuizQuestion('Tree is to forest as star is to', [
+    'galaxy', 'planet', 'moon', 'sky'
+  ]),
+  QuizQuestion('Doctor is to patient as teacher is to', [
+    'student', 'school', 'classroom', 'lesson'
+  ]),
+  QuizQuestion('Fish is to water as bird is to', [
+    'air', 'wing', 'sky', 'tree'
+  ]),
+  QuizQuestion('Camera is to photograph as microscope is to', [
+    'microbe', 'science', 'lens', 'magnify'
+  ]),
+  QuizQuestion('Farmer is to crops as miner is to', [
+    'minerals', 'tools', 'gold', 'soil'
+  ]),
+  QuizQuestion('Author is to book as composer is to', [
+    'music', 'symphony', 'melody', 'note'
+  ])
 ];
